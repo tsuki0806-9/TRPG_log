aa2a870f95261fa48750f20e85d77e200ef9bb7f
tsuki0806-9 <tsuki0983514583@gmail.com>
2025-08-31 22:42:07 +0800

    Update index.js

--- coffee and cat/index.js
+++ coffee and cat/index.js
@@ -2,5 +2,5 @@ var TITLE = "咖啡與貓";
 var INDEX = [
 	{	title: "團錄", url: "0708-coffee and cat.html" },
 	{	title: "KPC角色卡", url: "https://docs.google.com/spreadsheets/d/1rYzTxLrQQfT9TScuCS90FnN312rHZ-QgJfR-xBB-RY8/edit?usp=sharing" },
-	{	title: "PC角色卡", url: "0708-coffee and cat.html" },
+	{	title: "PC角色卡", url: "https://docs.google.com/spreadsheets/d/e/2PACX-1vS_Ilx3zvPgkMXDKxIfco9Z_DJgk9ncESQSyrjdq-RtLjlT5MxeXS8czz6l-hOOMA/pubhtml?gid=2142838181&single=true" },
 ];
