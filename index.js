437cc5b79ed1653410310370489004f8ddb1cfcb
tsuki0806-9 <tsuki0983514583@gmail.com>
2025-08-31 13:59:02 +0800

    .

--- index.js
+++ index.js
@@ -11,5 +11,17 @@ var INDEX = [
 			startDate: "2025/07/08",
 		}
 	},
+	{
+		system: "CoC",
+		title: "咖啡與貓",
+		url: "coffee and cat",
+		imgUrl: "src/img/replay-cover/example.jpg",
+		info: {
+			system: "CoC",
+			gm: "默月",
+			member: "千行",
+			startDate: "2025/07/08",
+		}
+	},
 
 ];
